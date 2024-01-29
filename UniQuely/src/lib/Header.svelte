783f74cbeb5d71d4a3e649c452ff50a31245a5cb
channacy <68759286+channacy@users.noreply.github.com>
2024-01-29 15:55:12 -0500

    added example header

---
+++ UniQuely/src/lib/Header.svelte
@@ -0,0 +1,12 @@
+<header>Hello world</header>
+
+<style>
+	header {
+		background-color: rebeccapurple;
+		color: ivory;
+		font-size: 2rem;
+		font-weight: bold;
+		padding-block: 1.5rem;
+		text-align: center;
+	}
+</style>
