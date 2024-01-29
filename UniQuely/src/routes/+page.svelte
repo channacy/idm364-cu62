cc31f1a66ecdf1a9a9ec0acf028fc7300162dbef
channacy <68759286+channacy@users.noreply.github.com>
2024-01-29 14:07:44 -0500

    setup scss

--- UniQuely/src/routes/+page.svelte
+++ UniQuely/src/routes/+page.svelte
@@ -1,12 +1,12 @@
-<html data-theme="cupcake">
+<html data-theme="cupcake" lang="sass">
   <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button class="btn">Hello daisyui</button>
-</html>
+      Hello world!
+    </h1>
+    <button class="btn">Hello daisyui</button>
 
   <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.green.100);
-    }
+      :global(html) {
+        background-color: theme(colors.green.100);
+      }
   </style>
+</html>
